b041eb451274527c464ae24dfddc6e00aa1cbea4
Bumler <hbulmer@callutheran.edu>
2017-12-15 00:08:56 -0800

    Finished(mostly) front end logic for trait compendium
    Need to add in the actual post but the logic and object are all there.
    Need to reset form after submission

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@ theSystem.controller('mainController', function($scope, $location){
 
 theSystem.controller('traitsController', function($scope, $http, $location){
 	apiBaseURL = "https://92faaba5-bf69-4269-aac4-187f38b840b7.mock.pstmn.io";
+	$scope.addingTrait = false;
 
 	$scope.goToMain = function(){
 		$location.path('/');
@@ -42,6 +43,24 @@ theSystem.controller('traitsController', function($scope, $http, $location){
 
 		for (i = 0; i < traitList.length; i++)
 			$scope.traits[i%3].push(traitList[i]);
+	}
+
+	$scope.addTrait = function(){
+		var newTrait = new Object();
+		newTrait.Name = $scope.newTraitForm.ntName.$viewValue;
+		newTrait.ActionType = $scope.newTraitForm.ntActionType.$viewValue;
+		newTrait.Points = $scope.newTraitForm.ntPoints.$viewValue;
+		newTrait.Description = $scope.newTraitForm.ntDescription.$viewValue;
+		newTrait.Cooldown = $scope.newTraitForm.ntCooldown.$viewValue;
+		newTrait.CooldownUnit = $scope.newTraitForm.ntCooldownUnit.$viewValue;
+
+		debugger;
+		//POST HERE
+		//ON SUCCESS
+		$scope.getTraits();
+		$scope.addingTrait = false;
+		//todo finish the reset form ng-if wont work for some reason
+		$scope.newTraitForm.$setPristine(true);
 	}
 
 	$scope.getTraits();
